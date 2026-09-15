5b3d3e7e0a2f00120174dd238cc0f852393ce5a8
Reza FKUY2023 <rahmadalfareza7@gmail.com>
2026-09-15 19:10:32 +0700

    Delete db directory

--- db/migrations/011_helper_role.sql
+++
@@ -1,4 +0,0 @@
--- Tambah role 'helper': akses Kelola Konten (UAB + UPI) tanpa akses manajemen pengguna/aktivitas/pengumuman.
-ALTER TABLE users DROP CONSTRAINT IF EXISTS users_role_check;
-ALTER TABLE users ADD CONSTRAINT users_role_check
-  CHECK (role IN ('student', 'admin', 'developer', 'helper'));
